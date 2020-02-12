a77849a7ba5e0477ed875ccc03d29c9389e093a7
christina kaliora <develop@pirron.one>
2020-02-12 19:49:46 +0100

    made gatekeeper middleware dynamic

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
 
-const hubsRouter = require('./hubs/hubs-router.js');
-const helmet = require('helmet');
+const hubsRouter = require("./hubs/hubs-router.js");
+const helmet = require("helmet");
 const server = express();
 
 //the three amigas: rachel, rita and nancy
 
-
 server.use(express.json());
 server.use(helmet());
-server.use('/api/hubs', gatekeeper, hubsRouter);
+server.use("/api/hubs", gatekeeper("mellon"), hubsRouter);
 
-server.get('/', logger, greeter, (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", logger, greeter, (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
@@ -28,15 +27,17 @@ function greeter(req, res, next) {
 }
 
 function logger(req, res, next) {
-  console.log(`${req.method} Request to ${req.originalUrl}`)
+  console.log(`${req.method} Request to ${req.originalUrl}`);
   next();
 }
 
-function gatekeeper(req, res, next){
-  if (req.headers.password && req.headers.password.toLowerCase() !== "mellon"){
-      res.status(401).json({error: "Wrong password!!!"});
+function gatekeeper(guess) {
+  return function(req, res, next) {
+    if (req.headers.password && req.headers.password.toLowerCase() === guess) {
+      next();
+    } else {
+      res.status(401).json({ error: "Wrong password!!!" });
       res.end();
-  } else {
-    next();
-  }
+    }
+  };
 }
